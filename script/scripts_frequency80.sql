0247998e102b81788c8f9e09ef619b9e55155a61
EstephanoBorovicz <estephano_borovicz@outlook.com>
2026-06-16 16:28:16 -0300

    crud imagem

--- script/scripts_frequency80.sql
+++ script/scripts_frequency80.sql
@@ -1,121 +1,7 @@
-#Objetivo: Este arquivo contém os CRUDs para os DAOs do projeto cafeteria
+#Objetivo: Este arquivo contém os CRUDs para os DAOs do projeto frequency80cafe
 #Autor: Estephano Borovicz
 #Data: 11/06/2026
 #Versão:1.0
-
-use db_frequency80cafe;
-#----------------------------------------select---------------------------------------
-#retorna todos os produtos
-select * from tbl_produto;
-
-#retorna um produto por id
-select * from tbl_produto where tbl_produto.id = '${produto.id}';
-
-#retorna todas as imagens
-select * from tbl_imagem;
-
-# retorna todas as imagens vinculadas a um produto
-select tbl_imagem.* from tbl_imagem
-inner join tbl_produto
-on tbl_produto.id = tbl_imagem.id_produto where tbl_produto.id = '${produto.id}';
-
-#retorna todas as categorias
-select * from tbl_categoria;
-
-#retorna todas as categorias vinculadas à um produto
-select * from tbl_categoria
-inner join tbl_produto_categoria
-on tbl_categoria.id =  tbl_produto_categoria.id_categoria
-inner join tbl_produto
-on tbl_produto.id = tbl_produto_categoria.id_produto
-where tbl_produto.id = '${produto.id}';
-
-#retorna todos os produtos vinculados a uma categoria
-select * from tbl_produto
-inner join tbl_produto_categoria
-on tbl_produto.id = tbl_produto_categoria.id_produto
-inner join tbl_categoria
-on tbl_categoria.id = tbl_produto_categoria.id_categoria
-where tbl_categoria.id = '${categoria.id}';
-
-# retorna todos os produtos, suas imagens vinculadas e suas categorias?
-select tbl_produto.id as id_produto, tbl_produto.nome as nome_produto, 
-tbl_produto.descricao as descricao_produto, tbl_produto.preco as preco_produto, 
-tbl_imagem.url as url_imagem, tbl_categoria.categoria as categoria_categoria 
-from tbl_produto
-
-inner join tbl_imagem
-on tbl_produto.id = tbl_imagem.id_produto
-
-inner join tbl_produto_categoria
-on tbl_produto.id = tbl_produto_categoria.id_produto
-
-inner join tbl_categoria
-on tbl_categoria.id = tbl_produto_categoria.id_categoria;
-
-#------------------------------insert-----------------------------------
-
- #insert novo admin
- insert into tbl_admin(
- nome_usuario,
- email,
- senha,
- jwt
- )VALUES(
-'${admin.nome_usuario}',
-'${admin.email}',
-'${admin.senha}',
-'${admin.jwt}'
- );
- 
-#insert novo produto
-insert into tbl_produto (nome, descricao, preco
-)VALUES(
-'${produto.nome}',
-'${produto.descricao}',
-'${produto.preco}'
- );
- 
- #insert nova imagem vinculada a um produto
- insert into tbl_imagem (
- url,
- id_produto
- )VALUES(
- '${imagem.url}',
- '${produto.id}'
- );
- 
- #insert nova categoria
- insert into tbl_categoria(
- categoria
- )VALUES(
- '${categoria.categoria}'
- );
- 
- #---------------------------update---------------------------------
- 
- #update produto por id
- update tbl_produto set
-                        nome             = '${filme.nome}',
-                        descricao        = '${filme.sinopse}',
-                        preco            = '${filme.capa}'
-                    where id = '${produto.id}';
-                    
-# update imagem por id
- update tbl_imagem set
-                        url             = '${imagem.url}'
-                    where id = '${imagem.id}';
-                    
-# update categoria por id
- update tbl_categoria set
-                        categoria       = '${categoria.categoria}'
-                    where id = '${categoria.id}';
-                    
-#------------------------------delete---------------------------------
-
-
-
-
 
 #-------------------------------------------------------admin----------------------------------------------------------------
  
@@ -266,6 +152,19 @@ delimiter $$
                 preco
         );
       END $$
+
+      #inserir produto na tbl_imagem
+    delimiter $$
+      create trigger tgrInsertProduto
+		before insert on tbl_produto
+			for each row
+				begin
+					insert into tbl_imagem (
+                    id_produto
+                    )values(
+                    tbl_produto.id
+                    );
+	end $$
     
 #listar todos os produtos cadastrados
       #model -> sql = `select * from vwCategoria`
@@ -316,5 +215,56 @@ end $$
           begin 
             delete from tbl_produto_categoria where id_produto = old.id_produto;
           end $$
+          
+          #-------------------------------------------------------imagem----------------------------------------------------------------
+ 
+  #inserir nova imagem
+        #model -> sql = `call procInsertImagem(${imagem.url},${imagem.id_produto}')`
+        delimiter $$
+          create procedure procInsertImagem(IN url varchar(255), id_produto int)
+            BEGIN
+              insert into tbl_imagem(
+                  url,
+                  tbl_imagem.id_produto
+              )VALUES(
+                  url,
+                  id_produto
+                  );
+        END $$
+ 
+ #listar todos as imagens
+      #model -> sql = `select * from vwImagem`
+        create view vwImagem  as
+          select * from tbl_imagem order by id desc;
+
+      #buscar imagem por id
+      #model -> sql = `call procSelectImagemById('${imagem.id}')`
+      delimiter $$
+        create procedure procSelectImagemById (in id int)
+          begin
+            select * from tbl_imagem where tbl_imagem.id = id;
+        end $$
+
+#atualizar imagem por id
+      #model -> sql = `call procUpdateImagemById('${imagem.id}','${imagem.url}','${imagem.id_produto}')`
+      delimiter $$
+        create procedure procUpdateImagemById(in id int, url varchar(255), id_produto int)
+          begin
+                update tbl_imagem set
+                                        url = new.url,
+                                        id_produto = new.id_produto
+                          where id = tbl_imagem.id;
+        end $$
+
+#deletar imagem por id
+ #model -> sql = `call procDeleteImagemById('${imagem.id}')`
+
+      delimiter $$
+      create procedure procDeleteImagemById(in id int)
+        begin
+          delete from tbl_imagem where tbl_imagem.id = id;
+        end$$
+          
+
           
       
